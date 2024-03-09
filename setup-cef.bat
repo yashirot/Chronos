4268f71d80fef5a948883c2bf4b077e1fa2c2e9e
YASHIRO Taketsugu <yashiro@clear-code.com>
2024-03-09 20:41:40 +0900

    Update CEFVAR to 122

--- setup-cef.bat
+++ setup-cef.bat
@@ -15,7 +15,7 @@ set BASEDIR=%~dp0
 IF NOT DEFINED CEFVER (
   echo Use the default CEF version.
   echo To build with a newer CEF version, set CEFVER explicitly.
-  set CEFVER=cef_binary_119.4.3+gc76a3b9+chromium-119.0.6045.159_windows32_minimal
+  set CEFVER=cef_binary_122.1.10+gc902316+chromium-122.0.6261.112_windows32_minimal
 )
 set CEFHOST=https://cef-builds.spotifycdn.com
 
